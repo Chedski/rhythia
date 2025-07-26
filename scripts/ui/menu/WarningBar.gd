b3e853d12433943d3d1d34c80eb9f93f7af4ec36
Chedski <plant@chedski.net>
2025-07-25 23:48:23 -0400

    wip(online): Add warning bar messages

--- scripts/ui/menu/WarningBar.gd
+++ scripts/ui/menu/WarningBar.gd
@@ -21,28 +21,64 @@ func display_warning(id:String):
 			color = Color("#5cb76e")
 		"smm":
 			state = true
-			$L.text = "Running in single map mode. Online features, PBs, and most map actions are disabled."
+			$L.text = tr("Running in single map mode. Online features, PBs, and most map actions are disabled.")
 			color = Color("#ffbb19")
+		"online_excluded":
+			state = true
+			$L.text = tr("Your account has been excluded. Online features, including score submission, are disabled. See rhythia.com/rules for more information.")
+			color = Color("#ff7c19")
+		"online_restricted":
+			state = true
+			$L.text = tr("Your account has been restricted. Scores will not be submitted. See rhythia.com/rules for more information.")
+			color = Color("#ffbb19")
+		"online_silenced":
+			state = true
+			$L.text = tr("Your account has been silenced. Chat and comments will be read-only. See rhythia.com/rules for more information.")
+			color = Color("#ffbb19")
+		"online_nointernet":
+			state = true
+			$L.text = tr("You are not connected to the internet. Score submission and other online features are unavailable.")
+			color = Color("#f2e830")
+		"online_outage":
+			state = true
+			$L.text = tr("Rhythia online services are experiencing an outage. Score submission and other online features are unavailable.")
+			color = Color("#f29e30")
+		"online_maintenance":
+			state = true
+			$L.text = tr("Rhythia online services are undergoing maintenance. Score submission and other online features are unavailable.")
+			color = Color("#f29e30")
+		"online_antitamper":
+			state = true
+			$L.text = tr("Your game has been modified, or you are using an unapproved build. Score submission is disabled. (Press C to dismiss)")
+			color = Color("#fc6d35")
+		"online_scoremaintenance":
+			state = true
+			$L.text = tr("Score submission is currently disabled for maintenance.")
+			color = Color("#f29e30")
+		"online_loggedout":
+			state = true
+			$L.text = tr("You are not logged into your Rhythia account.")
+			color = Color("#f2e6d7")
 		"spawn":
 			state = true
-			$L.text = "Note spawn effects may be buggy."
+			$L.text = tr("Note spawn effects may be buggy.")
 			color = Color("#fc794e")
 		"debug":
 			state = true
-			$L.text = "Development mode is active, game mods will not be used."
+			$L.text = tr("Development mode is active, game mods will not be used and scores will not be submitted.")
 			color = Color("#477d94")
 		"controllercheck":
 			state = true
-			$L.text = "Controller checking disabled. Press C to hide this bar."
+			$L.text = tr("Controller checking disabled. Press C to hide this bar.")
 			color = Color("#8400ff")
 		"experimental":
 			state = true
-			$L.text = "You are currently using experimental settings. Expect bugs."
+			$L.text = tr("You are currently using experimental settings. Expect bugs.")
 			color = Color("#c1c1ac")
 #			color = Color("#dbd1a2")
 		"experimental_mod":
 			state = true
-			$L.text = "One or more mods you selected are currently experimental. Expect bugs."
+			$L.text = tr("One or more mods you selected are currently experimental. Expect bugs.")
 			color = Color("#77c1d1")
 		_:
 			assert(false)
@@ -58,14 +94,38 @@ func check_experimental_settings():
 		if Rhythia.get(k): return true
 	return false
 
+var test_excluded = false
+var test_restricted = false
+var test_silenced = false
+var online_status = "ok"
+# antitamper / loggedout / nointernet / scoremaintenance / maintenance / outage / ok
+
 func check_warnings():
-	if Input.is_action_pressed("arcw3") and bar_enable:
+	if Input.is_action_pressed("dismiss_bar") and bar_enable:
 		bar_enable = false
 
 	if Input.is_action_pressed("warning_test"):
 		return "test"
 	elif Rhythia.single_map_mode:
 		return "smm"
+	elif test_excluded:
+		return "online_excluded"
+	elif test_restricted:
+		return "online_restricted"
+	elif test_silenced:
+		return "online_silenced"
+	elif online_status == "nointernet":
+		return "online_nointernet"
+	elif online_status == "outage":
+		return "online_outage"
+	elif online_status == "maintenance":
+		return "online_maintenance"
+	elif bar_enable and online_status == "antitamper" and !OS.has_feature("debug"):
+		return "online_antitamper"
+	elif online_status == "scoremaintenance":
+		return "online_scoremaintenance"
+	elif online_status == "loggedout":
+		return "online_loggedout"
 	elif Rhythia.note_spawn_effect:
 		return "spawn"
 	# Temp mark HR as a Experimental due to issues :/
@@ -75,11 +135,8 @@ func check_warnings():
 		return "debug"
 	elif check_experimental_settings():
 		return "experimental"
-	elif Rhythia.ignore_controller_detection:
-		if bar_enable:
-			return "controllercheck"
-		else:
-			return "none"
+	elif bar_enable and Rhythia.ignore_controller_detection:
+		return "controllercheck"
 	else:
 		return "none"
 
